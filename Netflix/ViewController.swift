417361633ddc946f101144b84a4e4252a844e459
vaGaidenko <va.gaidenko@gmail.com>
2022-05-27 20:40:05 +0700

    Initial Commit

---
+++ Netflix/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Netflix
+//
+//  Created by Владислав Гайденко on 27.05.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
